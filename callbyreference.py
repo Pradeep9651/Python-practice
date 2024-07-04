627e8bf26e374ce16f361ffee027a524aef5e74d
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-07-04 12:53:37 +0530

    prctc6,7

---
+++ callbyreference.py
@@ -0,0 +1,6 @@
+def edtech(set):
+    set.add(56)
+    print("inside",set)
+set1={23,45,69,34}
+edtech(set1)
+print("outside",set1)
---
+++ callbyvalue.py
@@ -0,0 +1,6 @@
+def edtech(string):
+    string="welcome to github"
+    print("inside",string)
+string="gl"
+print("outside",string)
+edtech(string)
